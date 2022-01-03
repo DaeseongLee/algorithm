3332fd4dede2c889963e0f98a256146c63bca3de
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2022-01-03 23:08:35 +0900

    postfixNotation

--- stack/postfixNotation.js
+++ stack/postfixNotation.js
@@ -1,2 +1,41 @@
 const fs = require('fs');
-const input = fs.readFileSync('../dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('../dev/stdin').toString().trim().split('');
+
+const stack = [];
+let result = '';
+for (let i = 0; i < input.length; i++) {
+    const str = input[i];
+    switch (str) {
+        case '(':
+            stack.push('(');
+            break;
+        case ')':
+            while (stack[stack.length - 1] !== '(') {
+                result += stack.pop();
+            }
+            stack.pop();
+            break;
+        case '*':
+        case '/':
+            while (stack.length > 0 && (stack[stack.length - 1] === '*' || stack[stack.length - 1] === '/')) {
+                result += stack.pop();
+            }
+            stack.push(str);
+            break;
+        case '+':
+        case '-':
+            while(stack.length > 0 && stack[stack.length - 1] !== '(') {
+                result += stack.pop();
+            }
+            stack.push(str);
+            break;
+        default:
+            result += str;
+            break;
+        }
+}
+
+while (stack.length > 0) {
+    result += stack.pop();
+}
+console.log(result);
